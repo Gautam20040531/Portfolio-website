068de6bdb6263e15bb11185653f9411cf69ab066
Gautam Pal <gp2285172@gmail.com>
2026-05-03 00:37:17 +0530

    Update WhatIDo section with Full-Stack and Cybersecurity skills

--- src/components/WhatIDo.tsx
+++ src/components/WhatIDo.tsx
@@ -58,6 +58,8 @@ const WhatIDo = () => {
               />
             </svg>
           </div>
+          
+          {/* First Box: Full Stack Development */}
           <div
             className="what-content what-noTouch"
             ref={(el) => setRef(el, 0)}
@@ -87,28 +89,28 @@ const WhatIDo = () => {
             <div className="what-corner"></div>
 
             <div className="what-content-in">
-              <h3>DEVELOP</h3>
+              <h3>FULL-STACK WEB</h3>
               <h4>Description</h4>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                quia aliquid laboriosam ducimus sit molestiae.
+                I engineer story-driven websites and robust, high-performance applications. From responsive frontends to complex backend systems, I build scalable digital products that deliver buttery-smooth user experiences.
               </p>
               <h5>Skillset & tools</h5>
               <div className="what-content-flex">
-                <div className="what-tags">JavaScript</div>
-                <div className="what-tags">TypeScript</div>
-                <div className="what-tags">Three.js</div>
-                <div className="what-tags">React</div>
-                <div className="what-tags">Css</div>
-                <div className="what-tags">Node.js</div>
                 <div className="what-tags">Next.js</div>
-                <div className="what-tags">Express.js</div>
-                <div className="what-tags">PHP</div>
-                <div className="what-tags">MySql</div>
+                <div className="what-tags">React</div>
+                <div className="what-tags">TypeScript</div>
+                <div className="what-tags">Node.js</div>
+                <div className="what-tags">MongoDB</div>
+                <div className="what-tags">Framer Motion</div>
+                <div className="what-tags">Tailwind CSS</div>
+                <div className="what-tags">C++</div>
+                <div className="what-tags">Flutter</div>
               </div>
               <div className="what-arrow"></div>
             </div>
           </div>
+          
+          {/* Second Box: Cybersecurity */}
           <div
             className="what-content what-noTouch"
             ref={(el) => setRef(el, 1)}
@@ -128,22 +130,20 @@ const WhatIDo = () => {
             </div>
             <div className="what-corner"></div>
             <div className="what-content-in">
-              <h3>DESIGN</h3>
+              <h3>CYBERSECURITY</h3>
               <h4>Description</h4>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
-                quia aliquid laboriosam ducimus sit molestiae
+                Deeply passionate about ethical hacking and digital security. I focus on finding vulnerabilities, securing web architectures, and understanding hardware manipulation (like Flipper Zero) to build applications that are safe by design.
               </p>
               <h5>Skillset & tools</h5>
               <div className="what-content-flex">
-                <div className="what-tags">Blender</div>
-                <div className="what-tags">Zbrush</div>
-                <div className="what-tags">UI Design</div>
-                <div className="what-tags">Motion</div>
-                <div className="what-tags">Rigging</div>
-                <div className="what-tags">3D Animation</div>
-                <div className="what-tags">Character Design</div>
-                <div className="what-tags">Modelling</div>
+                <div className="what-tags">Ethical Hacking</div>
+                <div className="what-tags">Network Security</div>
+                <div className="what-tags">Parrot OS</div>
+                <div className="what-tags">Penetration Testing</div>
+                <div className="what-tags">IoT Hardware</div>
+                <div className="what-tags">ESP32 & RTL-SDR</div>
+                <div className="what-tags">Vulnerability Assessment</div>
               </div>
               <div className="what-arrow"></div>
             </div>
